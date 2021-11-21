42abff38f9ecd2a5335b3e3f0de4ec94989e67a3
Han <hanwen.zhu@mail.mcgill.ca>
2021-11-20 20:42:43 -0500

    2021/11/20

--- Checker.hs
+++ Checker.hs
@@ -81,24 +81,24 @@ extractIdent exLst acc = case exLst of
         Ident _ var -> extractIdent xs (acc ++ [x])
         _ -> extractIdent xs acc
 
-checkNullPtr :: String -> [Expr] -> [Stmt] -> String
+checkNullPtr :: [String] -> [Expr] -> [Stmt] -> [String]
 checkNullPtr acc notNull stmts = case stmts of
     [] -> acc
     (x:xs) -> case x of
         IfStmt _ cond trBr (Just elBr) -> case notNull `intersect` extractIdent [cond] [] of
             [] -> checkNullPtr (acc ++ checkNullPtr [] notNull [trBr] ++ checkNullPtr [] notNull [elBr]) notNull xs
-            (y:ys) -> checkNullPtr (acc ++ checkNullPtr [] notNull [trBr] ++ checkNullPtr [] notNull [elBr] ++ concatMap (\z -> "dereferencing a pointer that could be null in the " ++ showStmtPos x ++ ": " ++ showExprPos z) (y:ys)) notNull xs
+            (y:ys) -> checkNullPtr (acc ++ checkNullPtr [] notNull [trBr] ++ checkNullPtr [] notNull [elBr] ++ [concatMap (\z -> "dereferencing a pointer that could be null in the " ++ showStmtPos x ++ ": " ++ showExprPos z) (y:ys)]) notNull xs
         IfStmt _ cond trBr Nothing -> case notNull `intersect` extractIdent [cond] [] of
             [] -> checkNullPtr (acc ++ checkNullPtr [] notNull [trBr]) notNull xs
-            (y:ys) -> checkNullPtr (acc ++ checkNullPtr [] notNull [trBr] ++ concatMap (\z -> "dereferencing a pointer that could be null in the " ++ showStmtPos x ++ ": " ++ showExprPos z) (y:ys)) notNull xs
+            (y:ys) -> checkNullPtr (acc ++ checkNullPtr [] notNull [trBr] ++ [concatMap (\z -> "dereferencing a pointer that could be null in the " ++ showStmtPos x ++ ": " ++ showExprPos z) (y:ys)]) notNull xs
         WhileStmt _ cond body -> case notNull `intersect` extractIdent [cond] [] of
             [] -> checkNullPtr (acc ++ checkNullPtr [] notNull [body]) notNull xs
-            (y:ys) -> checkNullPtr (acc ++ checkNullPtr [] notNull [body] ++ concatMap (\z -> "dereferencing a pointer that could be null in the " ++ showStmtPos x ++ ": " ++ showExprPos z) (y:ys)) notNull xs
+            (y:ys) -> checkNullPtr (acc ++ checkNullPtr [] notNull [body] ++ [concatMap (\z -> "dereferencing a pointer that could be null in the " ++ showStmtPos x ++ ": " ++ showExprPos z) (y:ys)]) notNull xs
         DoWhileStmt _ body _ -> checkNullPtr (acc ++ checkNullPtr [] notNull [body]) notNull xs
         ForStmt _ _ _ _ body -> checkNullPtr (acc ++ checkNullPtr [] notNull [body]) notNull xs
         SwitchStmt _ cond body -> case notNull `intersect` extractIdent [cond] [] of
             [] -> checkNullPtr (acc ++ checkNullPtr [] notNull [body]) notNull xs
-            (y:ys) -> checkNullPtr (acc ++ checkNullPtr [] notNull [body] ++ concatMap (\z -> "dereferencing a pointer that could be null in the " ++ showStmtPos x ++ ": " ++ showExprPos z) (y:ys)) notNull xs
+            (y:ys) -> checkNullPtr (acc ++ checkNullPtr [] notNull [body] ++ [concatMap (\z -> "dereferencing a pointer that could be null in the " ++ showStmtPos x ++ ": " ++ showExprPos z) (y:ys)]) notNull xs
         CaseStmt _ _ body -> checkNullPtr (acc ++ checkNullPtr [] notNull [body]) notNull xs
         DfltStmt _ body -> checkNullPtr (acc ++ checkNullPtr [] notNull [body]) notNull xs
         FnDecStmt _ _ _ body -> checkNullPtr (acc ++ checkNullPtr [] notNull [body]) notNull xs
@@ -106,73 +106,79 @@ checkNullPtr acc notNull stmts = case stmts of
         BodyStmt stmts -> checkNullPtr acc notNull (stmts ++ xs)
         _ -> checkNullPtr acc notNull xs
 
-checkFor :: Stmt -> String
-checkFor stmt = case stmt of
-    IfStmt _ _ trBr (Just elBr) -> checkFor trBr ++ checkFor elBr
-    IfStmt _ _ trBr Nothing -> checkFor trBr
-    WhileStmt _ end body -> checkFor body
-    DoWhileStmt _ body end -> checkFor body
-    ForStmt pos _ end up body -> case end of
-        BinOp binSym _ _ -> case binSym of
-            Lt -> lessDec up body
-            Lte -> lessDec up body
-            Gt -> moreInc up body
-            Gte -> moreInc up body
-            _ -> ""
-        _ -> ""
-    SwitchStmt _ _ body -> checkFor body
-    CaseStmt _ _ body -> checkFor body
-    DfltStmt _ body -> checkFor body
-    FnDecStmt _ _ _ body -> checkFor body
-    BodyStmt stmts -> intercalate "" $ checkFor <$> stmts
-    _ -> ""
-    where lessDec up body = case up of
+checkFor :: [String] -> [Stmt] -> [String]
+checkFor acc stmts = case stmts of
+    [] -> acc
+    (x:xs) -> case x of
+        IfStmt _ _ trBr (Just elBr) -> checkFor (acc ++ checkFor [] [trBr] ++ checkFor [] [elBr]) xs
+        IfStmt _ _ trBr Nothing -> checkFor (acc ++ checkFor [] [trBr]) xs
+        WhileStmt _ end body -> checkFor (acc ++ checkFor [] [body]) xs
+        DoWhileStmt _ body end -> checkFor (acc ++ checkFor [] [body]) xs
+        ForStmt pos _ end up body -> case end of
+            BinOp binSym _ _ -> case binSym of
+                Lt -> lessDec up x body xs
+                Lte -> lessDec up x body xs
+                Gt -> moreInc up x body xs
+                Gte -> moreInc up x body xs
+                _ -> checkFor acc xs
+            _ -> checkFor acc xs
+        SwitchStmt _ _ body -> checkFor (acc ++ checkFor [] [body]) xs
+        CaseStmt _ _ body -> checkFor (acc ++ checkFor [] [body]) xs
+        DfltStmt _ body -> checkFor (acc ++ checkFor [] [body]) xs
+        FnDecStmt _ _ _ body -> checkFor (acc ++ checkFor [] [body]) xs
+        BodyStmt body -> checkFor acc (body ++ xs)
+        _ -> checkFor acc xs
+    where lessDec up forStmt body rem = case up of
             UnaOp unaSym _ -> case unaSym of
-                    PreDec -> "suspicious loop condition: " ++ showStmtPos stmt ++ "\n" ++ checkFor body
-                    PostDec -> "suspicious loop condition: " ++ showStmtPos stmt ++ "\n" ++ checkFor body
-                    _ -> checkFor body
-            _ -> ""
-          moreInc up body = case up of
+                    PreDec -> checkFor (acc ++ ["suspicious loop condition: " ++ showStmtPos forStmt] ++ checkFor [] [body]) rem
+                    PostDec -> checkFor (acc ++ ["suspicious loop condition: " ++ showStmtPos forStmt] ++ checkFor [] [body]) rem
+                    _ -> checkFor (acc ++ checkFor [] [body]) rem
+            _ -> checkFor acc rem
+          moreInc up forStmt body rem = case up of
                 UnaOp unaSym _ -> case unaSym of
-                    PreInc -> "suspicious loop condition: " ++ showStmtPos stmt ++ "\n" ++ checkFor body
-                    PostInc -> "suspicious loop condition: " ++ showStmtPos stmt ++ "\n" ++ checkFor body
-                    _ -> checkFor body
-                _ -> ""
+                    PreInc -> checkFor (acc ++ ["suspicious loop condition: " ++ showStmtPos forStmt] ++ checkFor [] [body]) rem
+                    PostInc -> checkFor (acc ++ ["suspicious loop condition: " ++ showStmtPos forStmt] ++ checkFor [] [body]) rem
+                    _ -> checkFor (acc ++ checkFor [] [body]) rem
+                _ -> checkFor acc rem
 
-redundBranch :: Stmt -> String
-redundBranch stmt = case stmt of
-    IfStmt {} -> checkRedund "branches" (removeElse stmt []) []
-    WhileStmt _ _ body -> redundBranch body
-    DoWhileStmt _ body _ -> redundBranch body
-    ForStmt _ _ _ _ body -> redundBranch body
-    SwitchStmt _ _ body -> case body of
-        BodyStmt stmts -> checkRedund "branches" (removeDflt stmts []) []
-        _ -> ""
-    FnDecStmt _ _ _ body -> redundBranch body
-    BodyStmt stmts -> intercalate "" $ redundBranch <$> stmts
-    _ -> ""
+redundBranch :: [String] -> [Stmt] -> [String]
+redundBranch acc stmts = case stmts of
+    [] -> acc
+    (x:xs) -> case x of
+        IfStmt {} -> redundBranch (acc ++ [checkRedund [] "branches" (removeElse x []) []]) xs
+        WhileStmt _ _ body -> redundBranch (acc ++ redundBranch [] [body]) xs
+        DoWhileStmt _ body _ -> redundBranch (acc ++ redundBranch [] [body]) xs
+        ForStmt _ _ _ _ body -> redundBranch (acc ++ redundBranch [] [body]) xs
+        SwitchStmt _ _ body -> case body of
+            BodyStmt body -> redundBranch (acc ++ [checkRedund [] "branches" (removeDflt body []) []]) xs
+            _ -> redundBranch acc xs
+        FnDecStmt _ _ _ body -> redundBranch (acc ++ redundBranch [] [body]) xs
+        BodyStmt body -> redundBranch acc (body ++ xs)
+        _ -> redundBranch acc xs
     where removeDflt stmts acc = case stmts of
             [] -> acc
             (x:xs) -> case x of
                 CaseStmt _ _ st -> removeDflt xs (acc ++ [st])
                 _ -> removeDflt xs acc
           removeElse stmt acc = case stmt of
-              IfStmt pos _ st (Just elBr) -> removeElse elBr (acc ++ [st])
-              IfStmt pos _ st Nothing -> acc ++ [st]
-              _ -> acc
+            IfStmt pos _ st (Just elBr) -> removeElse elBr (acc ++ [st])
+            IfStmt pos _ st Nothing -> acc ++ [st]
+            _ -> acc
 
-redundCond :: Stmt -> String
-redundCond stmt = case stmt of
-    IfStmt {} -> checkRedund "conditions" (removeElse stmt []) []
-    WhileStmt _ _ body -> redundCond body
-    DoWhileStmt _ body _ -> redundCond body
-    ForStmt _ _ _ _ body -> redundCond body
-    SwitchStmt _ _ body -> case body of
-        BodyStmt stmts -> checkRedund "conditions" (removeDflt stmts []) []
-        _ -> ""
-    FnDecStmt _ _ _ body -> redundCond body
-    BodyStmt stmts -> intercalate "" $ redundCond <$> stmts
-    _ -> ""
+redundCond :: [String] -> [Stmt] -> [String]
+redundCond acc stmts = case stmts of
+    [] -> acc
+    (x:xs) -> case x of
+        IfStmt {} -> redundCond (acc ++ [checkRedund [] "conditions" (removeElse x []) []]) xs
+        WhileStmt _ _ body -> redundCond (acc ++ redundCond [] [body]) xs
+        DoWhileStmt _ body _ ->  redundCond (acc ++ redundCond [] [body]) xs
+        ForStmt _ _ _ _ body ->  redundCond (acc ++ redundCond [] [body]) xs
+        SwitchStmt _ _ body -> case body of
+            BodyStmt body -> redundCond (acc ++ [checkRedund [] "conditions" (removeDflt body []) []]) xs
+            _ -> redundCond acc xs
+        FnDecStmt _ _ _ body -> redundCond (acc ++ redundCond [] [body]) xs
+        BodyStmt body -> redundCond acc (body ++ xs)
+        _ -> redundCond acc xs
     where removeDflt stmts acc = case stmts of
             [] -> acc
             (x:xs) -> case x of
@@ -183,11 +189,11 @@ redundCond stmt = case stmt of
               IfStmt pos cond _ Nothing -> acc ++ [stmt]
               _ -> acc
 
-checkRedund :: String -> [Stmt] -> [Stmt] -> String
-checkRedund tp body seen = case body of
-    [] -> ""
-    (x:xs) -> if x `elem` seen then "redundant " ++ tp ++ ": " ++ showStmtPos x ++ "\n" ++ checkRedund tp xs seen
-              else checkRedund tp xs (x:seen)
+checkRedund :: String -> String -> [Stmt] -> [Stmt] -> String
+checkRedund acc tp body seen = case body of
+    [] -> acc
+    (x:xs) -> if x `elem` seen then checkRedund (acc ++ "redundant " ++ tp ++ ": " ++ showStmtPos x) tp xs seen
+              else checkRedund acc tp xs (x:seen)
 
 showStmtPos :: Stmt -> String
 showStmtPos st = case st of
@@ -210,22 +216,21 @@ showStmtPos st = case st of
 
 showExprPos :: Expr -> String
 showExprPos ex = case ex of
-    PtrMemAcc pos _ _ -> "pointer member access expression at " ++ show pos ++ "\n"
-    MemAcc pos _ _ -> "member access expression at " ++ show pos ++ "\n"
-    ArrAcc pos _ _ -> "array element access expression at " ++ show pos ++ "\n"
-    Ident pos var -> "variable " ++ var ++ " at " ++ show pos ++ "\n"
-    Call pos name _ -> "function call (function name: " ++ show name ++ " ) at " ++ show pos ++ "\n"
-    Atom pos val -> "atomic value " ++ show val ++ " at " ++ show pos ++ "\n"
-    _ -> "" ++ "\n"
+    PtrMemAcc pos _ _ -> "pointer member access expression at " ++ show pos
+    MemAcc pos _ _ -> "member access expression at " ++ show pos
+    ArrAcc pos _ _ -> "array element access expression at " ++ show pos
+    Ident pos var -> "variable " ++ var ++ " at " ++ show pos
+    Call pos name _ -> "function call (function name: " ++ show name ++ " ) at " ++ show pos
+    Atom pos val -> "atomic value " ++ show val ++ " at " ++ show pos
+    _ -> ""
 
-check :: String -> IO String
-check file = liftM2 (++) (fmap (checkNullPtr "" []) (parseFile file))
-    (liftM2 (++) (fmap (intercalate "" . removeEmp [] . fmap checkFor) (parseFile file))
-    (liftM2 (++) (fmap (intercalate "" . removeEmp [] . fmap redundBranch) (parseFile file))
-                        (fmap (intercalate "" . removeEmp [] . fmap redundCond) (parseFile file))))
-    where removeEmp acc lst = case lst of
-            [] -> acc
-            (x:xs) -> if x == "" then removeEmp acc xs else removeEmp (acc ++ [x]) xs
+check :: [[Stmt]] -> String
+check stmts = case concat $ unlines . filter (not . null) <$> ([checkFor [], redundBranch [], redundCond [], checkNullPtr [] []] <*> stmts) of 
+    "" -> "The checker didn't find any issues!"
+    other -> other
 
 checkFile :: IO ()
-checkFile = getLine >>= check >>= writeFile "report.txt" >> putStrLn "checking completed"
+checkFile = do
+    stmts <- getLine >>= parseFile
+    writeFile "report.txt" (check [stmts])
+    putStrLn "checking completed"
